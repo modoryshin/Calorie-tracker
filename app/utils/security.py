6efa2c55b56941e4e3cbb66b80b65328473f8351
modoryshin <doryshin19@yandex.ru>
2025-01-28 22:41:38 +0500

    variable name

--- app/utils/security.py
+++ app/utils/security.py
@@ -8,14 +8,14 @@ api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
 load_dotenv(find_dotenv())
 
-API_KEY = os.getenv('API_KEY')
+api_key = os.getenv('API_KEY')
 
 async def get_api_key(api_key_header: str = Security(api_key_header)) -> str | None:
-    API_KEY = os.getenv('API_KEY')
-    if not API_KEY:
+    print(api_key)
+    if not api_key:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='No API Key detected')
         
-    if api_key_header == API_KEY:
+    if api_key_header == api_key:
         return api_key_header
     else:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Could not validate credentials')
